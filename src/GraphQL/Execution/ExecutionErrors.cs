5601ad83f18bc20090cd932add6ddc9a28d6885f
Sebastian Bolling <sebastian.bolling@bookbeat.com>
2017-08-27 19:49:24 +0200

    added project

---
+++ src/GraphQL/Execution/ExecutionErrors.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace GraphQL
+{
+    public class ExecutionErrors : IEnumerable<ExecutionError>
+    {
+        private readonly List<ExecutionError> _errors = new List<ExecutionError>();
+
+        public void Add(ExecutionError error)
+        {
+            _errors.Add(error);
+        }
+
+        public void AddRange(IEnumerable<ExecutionError> errors)
+        {
+            _errors.AddRange(errors);
+        }
+
+        public int Count => _errors.Count;
+
+        public IEnumerator<ExecutionError> GetEnumerator()
+        {
+            return _errors.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ src/GraphQL/Execution/ExecutionResultJsonConverter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace GraphQL
+{
+    public class ExecutionResultJsonConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is ExecutionResult)
+            {
+                var result = (ExecutionResult) value;
+
+                writer.WriteStartObject();
+
+                writeData(result, writer, serializer);
+                writeErrors(result.Errors, writer, serializer, result.ExposeExceptions);
+
+                writer.WriteEndObject();
+            }
+        }
+
+        private void writeData(ExecutionResult result, JsonWriter writer, JsonSerializer serializer)
+        {
+            var data = result.Data;
+
+            if (result.Errors?.Any() == true && data == null)
+            {
+                return;
+            }
+
+            writer.WritePropertyName("data");
+            serializer.Serialize(writer, data);
+        }
+
+        private void writeErrors(ExecutionErrors errors, JsonWriter writer, JsonSerializer serializer, bool exposeExceptions)
+        {
+            if (errors == null || !errors.Any())
+            {
+                return;
+            }
+
+            writer.WritePropertyName("errors");
+
+            writer.WriteStartArray();
+
+            errors.Apply(error =>
+            {
+                writer.WriteStartObject();
+
+                writer.WritePropertyName("message");
+                if (exposeExceptions)
+                {
+                    serializer.Serialize(writer, error.ToString()); // return StackTrace (including all inner exceptions)
+                }
+                else
+                {
+                    serializer.Serialize(writer, error.Message);
+                }
+
+                if (error.Locations != null)
+                {
+                    writer.WritePropertyName("locations");
+                    writer.WriteStartArray();
+                    error.Locations.Apply(location =>
+                    {
+                        writer.WriteStartObject();
+                        writer.WritePropertyName("line");
+                        serializer.Serialize(writer, location.Line);
+                        writer.WritePropertyName("column");
+                        serializer.Serialize(writer, location.Column);
+                        writer.WriteEndObject();
+                    });
+                    writer.WriteEndArray();
+                }
+
+                writer.WriteEndObject();
+            });
+
+            writer.WriteEndArray();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(ExecutionResult);
+        }
+    }
+}
